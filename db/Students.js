46b09e0e28be033f7c31662feb677e0352a4b6e4
mailan97 <mailan090713@gmail.com>
2022-02-27 22:21:33 +0700

    lan

---
+++ db/Students.js
@@ -0,0 +1,32 @@
+﻿[
+    {
+        "username": "lanvtm",
+        "password": "123456",
+        "fullname": "Vũ Thị Mai Lan",
+        "email": "lanvtmph13907@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2002-12-17",
+        "schoolfee": "1500000",
+        "marks": "0"
+    },
+    {
+        "username": "vientt",
+        "password": "12345",
+        "fullname": "Trần Triết Viễn",
+        "email": "vientt@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "2002-10-11",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "trangtt",
+        "password": "1234",
+        "fullname": "Trần Thị Trang",
+        "email": "trangtt@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2001-05-15",
+        "schoolfee": "2000000",
+        "marks": "0"
+    }
+]
---
+++ js/myapp.js
@@ -0,0 +1,148 @@
+const app = angular.module("myapp",["ngRoute"]);
+
+app.config(function ($routeProvider){
+    // $locationProvider.hashPrefix("");
+    $routeProvider
+        .when("/dangnhap", {
+            templateUrl: 'dangnhap.html',
+            controller: 'dn_Ctrl'
+        })
+        .when("/dangky", {
+            templateUrl: 'dangky.html',
+            controller: 'user_Ctrl'
+        })
+        .when("/doimk", {
+            templateUrl: 'doimk.html',
+        })
+        .when("/quenmk", {
+            templateUrl: 'quenmk.html',
+        })
+        .when("/doimk", {
+            templateUrl: 'doimk.html',
+        })
+        .when("/capnhaths", {
+            templateUrl: 'capnhaths.html',
+        })
+        .when("/gioithieu", {
+            templateUrl: 'gioithieu.html',
+        })
+        .when("/lienhe", {
+            templateUrl: 'lienhe.html',
+        })
+        .when("/gopy", {
+            templateUrl: 'gopy.html',
+        })
+        .when("/hoidap", {
+            templateUrl: 'hoidap.html',
+        })
+        .when("/admin", {
+            templateUrl: 'admin.html',
+        });
+       
+});
+
+function subjectCtrl($scope, $http) {
+    $scope.list_subject = [];
+    $http.get('db/Subjects.js').then(function (response) {
+        $scope.list_subject = response.data
+    });
+}
+function studentCtrl($scope, $http) {
+    $scope.list_student = [];
+    $http.get('db/Students.js').then(function (response) {
+        $scope.list_student = response.data;
+    });
+}
+
+function userCtrl($scope, $rootScope, $http) {
+    $rootScope.dk = false;
+    $scope.students = [];
+    $scope.student = {};
+    $scope.isLoading = false;
+    const api = 'https://62149a6789fad53b1f187b5e.mockapi.io/user';
+
+    $scope.isLoading = true;
+    $http.get(api)
+        .then(function (response) {
+            $scope.isLoading = false;
+            $scope.students = response.data;
+        })
+        .catch(function (error) {
+            console.error(error);
+            $scope.isLoading = false;
+        });
+
+    $scope.dangky = function () {
+        // event.preventDefault();
+        var data = {
+            username: $scope.user.username,
+            password: $scope.user.password,
+            fullname: $scope.user.fullname,
+            sdt: $scope.user.sdt,
+            email: $scope.user.email,
+            gioi_tinh: $scope.user.gioi_tinh
+        }
+        $scope.isLoading = true;
+        $http.post(api, data)
+            .then(function (response) {
+                $scope.isLoading = false;
+                const sv = response.data;
+                $scope.students.push(sv);
+                alert('Đăng ký thành công');
+                document.location = "dangnhap.html";
+            });
+
+    }
+}
+
+function dnCtrl($scope, $rootScope, $http) {
+    $rootScope.dn = false;
+    $scope.students = [];
+    $scope.student = {};
+    $scope.fullname = '';
+    $scope.inStatus = false;
+    const api = 'https://62149a6789fad53b1f187b5e.mockapi.io/user';
+
+    $http.get(api)
+        .then(function (response) {
+            $scope.students = response.data;     
+            console.log(response.data);       
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+
+    $scope.dangnhap = function () {      
+        tb = false;
+        
+        
+        let a = $scope.user.username;
+        let b = $scope.user.password;
+        for (let i = 0; i < $scope.students.length; i++) {
+            if (a == $scope.students[i].username && b == $scope.students[i].password) {
+                tb = true;
+                $rootScope.student = $scope.students[i];
+                $scope.fullname = $scope.students[i].fullname;
+                break;
+                // $rootScope.dn = true;
+            }             
+        }
+        if (tb==true) {
+            alert("Đăng nhập thành công");
+            
+            $scope.inStatus = true;           
+            // document.location = "index.html";
+            console.log($scope.fullname);
+        } else {           
+            alert("Đăng nhập thất bại");
+        }
+    }
+}
+
+
+
+
+app.controller("subject_Ctrl", subjectCtrl);
+app.controller("student_Ctrl", studentCtrl);
+app.controller("user_Ctrl", userCtrl);
+app.controller("dn_Ctrl", dnCtrl);
