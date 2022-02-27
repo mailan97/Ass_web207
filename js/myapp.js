b11da9d3c85dd423a3ad7582ecf6053fb4de112e
mailan97 <mailan090713@gmail.com>
2022-02-28 01:27:09 +0700

    lan

--- js/myapp.js
+++ js/myapp.js
@@ -41,6 +41,7 @@ app.config(function($routeProvider) {
         })
         .when("/admin", {
             templateUrl: 'admin.html',
+            controller: 'qlus_Ctrl'
         });
 
 });
@@ -94,7 +95,7 @@ function userCtrl($scope, $rootScope, $http) {
                 const sv = response.data;
                 $scope.students.push(sv);
                 alert('Đăng ký thành công');
-                document.location = "dangnhap.html";
+                document.location = "index.html#!/dangnhap";
             });
 
     }
@@ -119,8 +120,6 @@ function dnCtrl($scope, $rootScope, $http) {
 
     $scope.dangnhap = function() {
         tb = false;
-
-
         let a = $scope.user.username;
         let b = $scope.user.password;
         for (let i = 0; i < $scope.students.length; i++) {
@@ -134,16 +133,92 @@ function dnCtrl($scope, $rootScope, $http) {
         }
         if (tb == true) {
             alert("Đăng nhập thành công");
-
             $scope.inStatus = true;
             document.location = "index.html#!/home";
-            console.log($scope.fullName);
         } else {
             alert("Đăng nhập thất bại");
         }
     }
 }
 
+function qlusCtrl($scope, $http) {
+    // Khởi tạo
+    $scope.students = [];
+    $scope.isSuccess = true;
+    $scope.message = "";
+    $scope.user = {
+        id: "",
+        username: "",
+        password: "",
+        fullname: "",
+        sdt: "",
+        email: "",
+        gioi_tinh: ""
+    };
+    $scope.isLoading = false;
+    const api = 'https://62149a6789fad53b1f187b5e.mockapi.io/user';
+
+    $scope.isLoading = true;
+    $http.get(api) // Gửi 1 request dạng GET lên API
+        .then(function (response) {
+            $scope.students = response.data;
+            $scope.totalPage = Math.ceil($scope.students.length / 10);
+            $scope.isLoading = false;
+        })
+        .catch(function (error) {
+            console.log(error);
+            $scope.isLoading = false;
+        });
+
+    // TODO
+    // if (index == -1) {
+    //     // them moi
+    // } else {
+    //     // cap nhat
+    // }
+
+    $scope.onSubmitForm = function (event) {
+        event.preventDefault();
+
+        $scope.isLoading = true;
+        // Gửi request dạng POST kèm data lên API
+        $http.post(api, $scope.user)
+            .then(function (response) {
+                // Tắt loading
+                $scope.isLoading = false;
+
+                // Thông báo
+                $scope.message = "Thêm mới thành công";
+                $scope.isSuccess = true;
+
+                // Thêm vào bảng
+                $scope.students.push(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+                $scope.isLoading = false;
+                $scope.message = "Thêm mới thất bại";
+                $scope.isSuccess = false;
+            });
+    }
+
+    $scope.onDelete = function (id, index) {
+        console.log(id);
+        const deleteAPI = api + '/' + id;
+        $scope.isLoading = true;
+        $http.delete(deleteAPI)
+            .then(function (response) {
+                $scope.isLoading = false;
+                $scope.students.splice(index, 1);
+                // splice -> xóa trong $scope.students
+
+                $("#modal_delete_" + id).modal('hide');
+            })
+            .catch(function (error) { 
+                console.error(error)
+            })
+    }
+};
 
 
 
@@ -151,3 +226,4 @@ app.controller("subject_Ctrl", subjectCtrl);
 app.controller("student_Ctrl", studentCtrl);
 app.controller("user_Ctrl", userCtrl);
 app.controller("dn_Ctrl", dnCtrl);
+app.controller("qlus_Ctrl", qlusCtrl);
